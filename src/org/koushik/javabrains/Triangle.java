655d749b1214810f61abcab9bcf8b3944c27ac1b
adilokas <adil.barrak@192.168.1.3>
2016-09-20 09:17:00 +0100

    First Commit

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -1,16 +1,71 @@
 package org.koushik.javabrains;
 
+/**
+ * @author adilb_000
+ *
+ */
 public class Triangle {
 
+	private Point PointA;
+	private Point PointB;
+	private Point PointC;
+	
+	
+	public Point getPointA() {
+		return PointA;
+	}
+
+
+	public void setPointA(Point pointA) {
+		PointA = pointA;
+	}
+
+
+	public Point getPointB() {
+		return PointB;
+	}
+
+
+	public void setPointB(Point pointB) {
+		PointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return PointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		PointC = pointC;
+	}
+
+
+	/*
 	private String type;
+	private int height;
+	public int getHeight() {
+		return height;
+	}
 	public String getType() {
 		return type;
 	}
-	public void setType(String type) {
+	public Triangle (String type,int height)
+	{
+		this.type=type;
+		this.height=height;
+	}/
+	
+/*	public void setType(String type) {
 		this.type = type;
-	}
+	}*/
 	public void draw(){
-		System.out.println(getType() +" Triangle draw");
+		//System.out.println(getType()+" "+getHeight()+" Triangle draw");
+		System.out.println("Point A=("+ getPointA().getX()+","+ getPointA().getY()+")");
+
+		System.out.println("Point B=("+ getPointB().getX()+","+ getPointB().getY()+")");
+		
+		System.out.println("Point C=("+ getPointC().getX()+","+ getPointC().getY()+")");
 	}
 
 }
